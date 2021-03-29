e3e42ca72982a649821ee90b5058c0d1da388f25
Alexandre Zanlorenzi <android.duim@gmail.com>
2021-03-29 19:25:41 -0300

    aula29

--- src/main/java/com/devdojo/springboot2/client/SpringClient.java
+++ src/main/java/com/devdojo/springboot2/client/SpringClient.java
@@ -38,6 +38,24 @@ public class SpringClient {
     samuraiChamplooSaved = new RestTemplate().exchange("http://localhost:8080/animes", HttpMethod.POST, new HttpEntity<>(samuraiChamploo, createJsonHeader()), Anime.class);
 
     log.info("saved anime {}", samuraiChamplooSaved);
+
+    Anime animeToBeUpdated = samuraiChamplooSaved.getBody();
+    animeToBeUpdated.setName("Samurai Champloo 2");
+
+    ResponseEntity<Void> samuraiChamplooUpdated = new RestTemplate().exchange("http://localhost:8080/animes",
+            HttpMethod.PUT,
+            new HttpEntity<>(animeToBeUpdated, createJsonHeader()),
+            Void.class);
+
+    log.info("updated anime {}", samuraiChamplooUpdated);
+
+    ResponseEntity<Void> samuraiChamplooDeleted = new RestTemplate().exchange("http://localhost:8080/animes/{id}",
+            HttpMethod.DELETE,
+            null,
+            Void.class,
+            animeToBeUpdated.getId());
+
+    log.info("Deleted anime {}", samuraiChamplooDeleted);
   }
 
   private static HttpHeaders createJsonHeader(){
