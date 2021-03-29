4931ef474a3cf1feeb48455b198765e23383cc00
Alexandre Zanlorenzi <android.duim@gmail.com>
2021-03-29 17:56:09 -0300

    error

--- src/main/java/com/devdojo/springboot2/mapper/AnimeMapper.java
+++ src/main/java/com/devdojo/springboot2/mapper/AnimeMapper.java
@@ -6,10 +6,10 @@ import com.devdojo.springboot2.requests.AnimePutRequestBody;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
-public abstract class AnimeMapper {
-  public static  final AnimeMapper INSTANCE = Mappers.getMapper(AnimeMapper.class);
-  public abstract Anime toAnime(AnimePostRequestBody animePostRequestBody);
+@Mapper
+public interface AnimeMapper {
+  AnimeMapper INSTANCE = Mappers.getMapper(AnimeMapper.class);
+  Anime toAnime(AnimePostRequestBody animePostRequestBody);
 
-  public abstract Anime toAnime(AnimePutRequestBody animePutRequestBody);
+  Anime toAnime(AnimePutRequestBody animePutRequestBody);
 }
